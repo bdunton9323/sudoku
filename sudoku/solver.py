39c01c791958bc686800a4949afd0fefa5e3cfa3
Benjamin Dunton <benjamin.dunton@gmail.com>
2022-01-17 15:53:59 -0500

    Moved StatsTracker to utils module

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -1,7 +1,7 @@
-import time
 from exceptions import ConstraintViolationError
 from solver_state import SolverState
 from utils import assert_constraint
+from utils import StatsTracker
 
 
 class Solver(object):
@@ -285,44 +285,3 @@ class BoardPrinter(object):
             if (i + 1) % 3 == 0 and i < len(row) - 1:
                 characters.append(' ')
         return ' '.join(characters)
-
-
-class StatsTracker(object):
-    def __init__(self):
-        self.num_iterations = None
-        self.num_guesses = 0
-        self.max_recursion_depth = 0
-        self.start_time = 0
-        self.end_time = 0
-
-    @property
-    def num_iterations(self):
-        return self._num_iterations
-
-    @num_iterations.setter
-    def num_iterations(self, val):
-        self._num_iterations = val
-
-    @property
-    def num_guesses(self):
-        return self._num_iterations
-
-    @num_guesses.setter
-    def num_guesses(self, val):
-        self._num_iterations = val
-
-    def on_recursion(self, depth):
-        if depth > self.max_recursion_depth:
-            self.max_recursion_depth = depth
-
-    def get_max_recursion_depth(self):
-        return self.max_recursion_depth
-
-    def start_timer(self):
-        self.start_time = time.time()
-
-    def stop_timer(self):
-        self.end_time = time.time()
-
-    def get_elapsed_time(self):
-        return self.end_time - self.start_time
--- sudoku/utils.py
+++ sudoku/utils.py
@@ -4,3 +4,44 @@ from exceptions import ConstraintViolationError
 def assert_constraint(condition, message="Game constraint violated"):
     if not condition:
         raise ConstraintViolationError(message)
+
+
+class StatsTracker(object):
+    def __init__(self):
+        self.num_iterations = None
+        self.num_guesses = 0
+        self.max_recursion_depth = 0
+        self.start_time = 0
+        self.end_time = 0
+
+    @property
+    def num_iterations(self):
+        return self._num_iterations
+
+    @num_iterations.setter
+    def num_iterations(self, val):
+        self._num_iterations = val
+
+    @property
+    def num_guesses(self):
+        return self._num_iterations
+
+    @num_guesses.setter
+    def num_guesses(self, val):
+        self._num_iterations = val
+
+    def on_recursion(self, depth):
+        if depth > self.max_recursion_depth:
+            self.max_recursion_depth = depth
+
+    def get_max_recursion_depth(self):
+        return self.max_recursion_depth
+
+    def start_timer(self):
+        self.start_time = time.time()
+
+    def stop_timer(self):
+        self.end_time = time.time()
+
+    def get_elapsed_time(self):
+        return self.end_time - self.start_time
